ccf76662262f9f653a7707c2401278c76e7d3ab4
yohan_rameau <yohan.rameau@gmail.com>
2022-04-20 11:09:27 +0200

    feat(multi_fenetre): write channel consumer - overwrites connect method

--- apps/activity/consumers.py
+++ apps/activity/consumers.py
@@ -1,10 +1,20 @@
 # activity/consumers.py
 import json
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 class ActivityConsumer(WebsocketConsumer):
     def connect(self):
-        print("Websocket :", "connect")
+        
+        self.activity_id = self.scope['url_route']['kwargs']['activity_id']
+        self.username = self.scope['url_route']['kwargs']['username']
+        self.group_name = f"{self.activity_id}_{self.username}"
+        
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name,
+            self.group_name
+        )
         self.accept()
 
     def disconnect(self, close_code):
